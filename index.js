482bd9ad6541de817fb1d679a0164129c03aa7fe
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2023-02-16 19:34:56 +0600

    get data and show data without function

---
+++ index.js
@@ -0,0 +1,33 @@
+// console.log('connected');
+
+// for 1st card btn
+document.getElementById('first-card').addEventListener('click', function(){
+    // console.log("1st btn clicked");
+
+
+    // get the data 
+
+    const productName = document.getElementById('first-name').innerText;
+    const productPrice = document.getElementById('first-price').innerText; 
+    const productQuantity = document.getElementById('first-quantity').innerText; 
+    console.log(productName, productPrice, productQuantity);
+
+    // calculating the price with quantity
+
+    const priceTotal = parseInt(productPrice) * parseInt(productQuantity);
+
+    // console.log( typeof priceTotal);
+
+    // show the data
+
+    const container = document.getElementById("table-container");
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+    <td>${1}</td>
+    <td>${productName}</td>
+    <td>${productPrice}</td>
+    <td>${productQuantity}</td>
+    <td>${priceTotal}</td> `;
+
+    container.appendChild(tr);
+})
